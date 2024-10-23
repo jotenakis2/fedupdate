1939f1fc50043d9317daa480853f8824c7f07e8e
jotenakis <>
2024-10-23 03:34:43 +0200

    update install script to hide errors

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo cp -f fedupdate /usr/local/bin/
-sudo cp -f post-upgrade-message.sh /usr/local/bin/
+sudo cp -f fedupdate /usr/local/bin/ 2>/dev/null
+sudo cp -f post-upgrade-message.sh /usr/local/bin/ 2>/dev/null
 sudo chmod 755 /usr/local/bin/fedupdate 
 sudo chmod 755 /usr/local/bin/post-upgrade-message.sh
-sudo cp fedupdate.1 /usr/local/share/man/man1/
-sudo mandb
+sudo cp fedupdate.1 /usr/local/share/man/man1/ 2>/dev/null
+sudo mandb >/dev/null 2>&1
 # Unités Systemd user
 mkdir -p "$HOME"/.config/systemd/user 2>/dev/null
-cp -f checkupdate.* "$HOME"/.config/systemd/user/
-cp -f postupgrade.service "$HOME"/.config/systemd/user/
+cp -f checkupdate.* "$HOME"/.config/systemd/user/ 2>/dev/null
+cp -f postupgrade.service "$HOME"/.config/systemd/user/ 2>/dev/null
 systemctl --user daemon-reload  
 # lance fedupdate -c -n pour faire un check seulement et notifier si une mise à jour est dispo
 systemctl --user enable checkupdate.service  
